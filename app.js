8f96daf0a7c389d21e87f6e72dce234fcc80cbed
xMartin <xmartin@siarp.de>
2013-02-15 12:34:31 +1100

    remove remoteStorage

--- app.js
+++ app.js
@@ -4,12 +4,6 @@
   var ulElement;
   var drinkRowPrefix = 'drinkrow-';
 
-  remoteStorage.util.setLogLevel('debug');
-
-  // remoteStorage.util.silenceAllLoggers();
-
-  // remoteStorage.util.unsilenceLogger('sync', 'store');
-
   function prefixId(id) {
     return drinkRowPrefix + id;
   }
@@ -22,56 +16,29 @@
     inputElement = formElement.getElementsByTagName('input')[0];
     ulElement = document.getElementById('drink-list');
 
-    remoteStorage.claimAccess('myfavoritedrinks', 'rw').
-      then(function() {
-        remoteStorage.displayWidget('remotestorage-connect');
+    ulElement.addEventListener('click', function(event) {
+      if(event.target.tagName === 'SPAN') {
+        removeDrink(unprefixId(event.target.parentNode.id));
+      }
+    });
 
-        remoteStorage.myfavoritedrinks.listDrinks().then(displayDrinks);
-
-        remoteStorage.onWidget('disconnect', function() {
-            emptyDrinks();
-        });
-
-        ulElement.addEventListener('click', function(event) {
-          if(event.target.tagName === 'SPAN') {
-            removeDrink(unprefixId(event.target.parentNode.id));
-          }
-        });
-
-        formElement.addEventListener('submit', function(event) {
-          event.preventDefault();
-          var trimmedText = inputElement.value.trim();
-          if(trimmedText) {
-            addDrink(trimmedText);
-          }
-          inputElement.value = '';
-        });
-
-        remoteStorage.myfavoritedrinks.on('change', function(event) {
-          // add
-          if(event.newValue && (! event.oldValue)) {
-            displayDrink(event.relativePath, event.newValue.name);
-          }
-          // remove
-          else if((! event.newValue) && event.oldValue) {
-            undisplayDrink(event.relativePath);
-          }
-        });
-      });
+    formElement.addEventListener('submit', function(event) {
+      event.preventDefault();
+      var trimmedText = inputElement.value.trim();
+      if(trimmedText) {
+        addDrink(trimmedText);
+      }
+      inputElement.value = '';
+    });
   }
 
   function addDrink(name) {
-    remoteStorage.myfavoritedrinks.addDrink(name);
+    var id = name.toLowerCase().replace(/\s|\//g, '-');
+    displayDrink(id, name);
   }
 
   function removeDrink(id) {
-    remoteStorage.myfavoritedrinks.removeDrink(id);
-  }
-
-  function displayDrinks(drinks) {
-    for(var drinkId in drinks) {
-      displayDrink(drinkId, drinks[drinkId].name);
-    }    
+    undisplayDrink(id);
   }
 
   function displayDrink(id, name) {
@@ -89,11 +56,6 @@
   function undisplayDrink(id) {
     var elem = document.getElementById(prefixId(id));
     ulElement.removeChild(elem);
-  }
-
-  function emptyDrinks() {
-    ulElement.innerHTML = '';
-    inputElement.value = '';
   }
 
   document.addEventListener('DOMContentLoaded', init);
